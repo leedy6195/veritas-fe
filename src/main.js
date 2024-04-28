31851f1097abe390492ebb9176c5aafb398776eb
Dongyun <leedy6195@naver.com>
2024-04-28 09:22:17 +0900

    fix: cookie credentials setting

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import {loadFonts} from './plugins/webfontloader'
+//import {loadFonts} from './plugins/webfontloader'
 import router from "@/scripts/router";
 import VueSimpleContextMenu from 'vue-simple-context-menu';
 import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
 
-loadFonts()
+//loadFonts()
 
 createApp(App)
     .use(router)
