27c72c23a369ecfe14e517c98f18012819be6616
dongyun <leedy6195@naver.com>
2024-04-29 21:16:28 +0900

    feat: inputmode none

--- src/pages/view/ReadingRoomView.vue
+++ src/pages/view/ReadingRoomView.vue
@@ -268,7 +268,7 @@ onMounted(() => {
   document.querySelector("input").focus();
   updateCurrentTime();
   setInterval(updateCurrentTime, 1000);
-  setInterval(onInputBlur, 1000)
+  setInterval(() => {window.scroll(0, 0)}, 1000)
 });
 </script>
 
