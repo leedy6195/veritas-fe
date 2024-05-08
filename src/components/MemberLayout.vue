6e1a78f56761def76747d5baa389cc93386a7102
dongyun <leedy6195@naver.com>
2024-05-08 12:19:29 +0900

    style: qrcode size

--- src/components/MemberLayout.vue
+++ src/components/MemberLayout.vue
@@ -1,20 +1,25 @@
 <template>
   <v-app-bar color="transparent" flat density="compact" v-if="$route.path !== '/login'">
     <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="!$vuetify.display.mdAndUp"></v-app-bar-nav-icon>
-    <v-toolbar-title><v-img src="@/assets/daechi-logo.png" max-height="40"></v-img></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-title>
+      <v-img src="@/assets/daechi-logo.png" max-height="40"></v-img>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
     <!--<v-btn class="logout-button">로그아웃</v-btn>-->
   </v-app-bar>
 
-  <v-navigation-drawer :location="'bottom'" style="height: auto" v-if="$route.path !== '/login'" v-model="drawer" floating :permanent="$vuetify.display.mdAndUp">
+  <v-navigation-drawer :location="'bottom'" style="height: auto" v-if="$route.path !== '/login'" v-model="drawer"
+                       floating :permanent="$vuetify.display.mdAndUp">
     <v-list density="compact" nav>
       <v-list-item prepend-icon="mdi-logout" title="로그아웃" value="logout" @click="logout"></v-list-item>
     </v-list>
   </v-navigation-drawer>
-
-  <router-view/>
+  <v-main>
+    <router-view/>
+  </v-main>
   <!-- footer -->
-  <v-footer class="footer" app>
+  <v-footer class="footer">
     <v-row>
       <v-col>
         <div>상호명: (주) 골든이스트에듀</div>
