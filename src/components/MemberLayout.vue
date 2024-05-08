574aa379b102e42ebde3be5716e6008ba49d2e2d
dongyun <leedy6195@naver.com>
2024-05-08 12:46:52 +0900

    style: qrcode size

--- src/components/MemberLayout.vue
+++ src/components/MemberLayout.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-app-bar color="transparent" flat density="compact" v-if="$route.path !== '/login'">
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="!$vuetify.display.mdAndUp"></v-app-bar-nav-icon>
-
+  <v-app-bar color="white" flat density="compact" v-if="$route.path !== '/login'">
     <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="!$vuetify.display.mdAndUp"></v-app-bar-nav-icon>
     <v-toolbar-title class="mx-auto text-center" style="position: absolute; left: 50%; transform: translateX(-50%);">
       <v-img src="@/assets/daechi-logo.png" max-height="40"></v-img>
