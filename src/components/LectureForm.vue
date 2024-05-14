bf28c5cbb1d294b1c1ed30d82895e6e8db14a1a9
Dongyun <leedy6195@naver.com>
2024-05-14 14:27:53 +0900

    feat: lecture & schedule update test

---
+++ src/components/LectureForm.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-form @submit.prevent="submit">
+    <v-row>
+      <v-col cols="12">
+        <v-text-field v-model="lecture.name" label="강의명" density="comfortable"></v-text-field>
+        <v-text-field v-model="lecture.instructor" label="강사명" density="comfortable"></v-text-field>
+        <v-text-field v-model="lecture.fee" label="수강료" type="number" density="comfortable"></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field v-model="lecture.startDate" label="시작일" type="date" density="comfortable"></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field v-model="lecture.endDate" label="종료일" type="date" density="comfortable"></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.MON.checked" label="월요일" value="월요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.MON.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.MON.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.MON.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.MON.checked"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.TUE.checked" label="화요일" value="화요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.TUE.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.TUE.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.TUE.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.TUE.checked"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.WED.checked" label="수요일" value="수요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.WED.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.WED.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.WED.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.WED.checked"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.THU.checked" label="목요일" value="목요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.THU.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.THU.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.THU.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.THU.checked"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.FRI.checked" label="금요일" value="금요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.FRI.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.FRI.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.FRI.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.FRI.checked"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.SAT.checked" label="토요일" value="토요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.SAT.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.SAT.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.SAT.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.SAT.checked"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-checkbox v-model="lecture.schedule.SUN.checked" label="일요일" value="일요일"></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.SUN.startTime" type="time" label="시작시간"
+                          :disabled="!lecture.schedule.SUN.checked"></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="lecture.schedule.SUN.endTime" type="time" label="종료시간"
+                          :disabled="!lecture.schedule.SUN.checked"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    required: true,
+  },
+})
+
+// eslint-disable-next-line no-undef
+const emit = defineEmits(['update:modelValue', 'submit'])
+
+const lecture = ref({ ...props.modelValue })
+
+const submit = () => {
+  emit('submit')
+}
+
+const daysOfWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+daysOfWeek.forEach(day => {
+  watch(() => lecture.value.schedule[day].checked, (newVal) => {
+    if (!newVal) {
+      lecture.value.schedule[day].startTime = null
+      lecture.value.schedule[day].endTime = null
+    }
+  })
+})
+
+watch(lecture, (newVal) => {
+  emit('update:modelValue', newVal)
+}, { deep: true })
+</script>
--- src/pages/admin/lecture/Lectures.vue
+++ src/pages/admin/lecture/Lectures.vue
@@ -8,7 +8,7 @@
 
     <v-row>
       <v-col>
-        <v-btn class="mb-4 float-right" color="primary" @click="addLectureDialog = true">
+        <v-btn class="mb-4 float-right" color="primary" @click="openAddLectureDialog">
           강의 추가
         </v-btn>
       </v-col>
@@ -24,6 +24,7 @@
             <th class="text-left">강의 일정</th>
             <th class="text-left">수강료</th>
             <th class="text-left">수강생 수</th>
+            <th class="text-left">수정</th>
           </tr>
           </thead>
           <tbody>
@@ -33,122 +34,22 @@
             <td>{{ lecture.startDate }} ~ {{ lecture.endDate }}</td>
             <td>{{ lecture.fee }}</td>
             <td>{{ lecture.enrolledStudents }}</td>
+            <td>
+              <v-btn color="primary" @click="openEditLectureDialog(lecture)">수정</v-btn>
+            </td>
           </tr>
           </tbody>
         </v-table>
       </v-col>
     </v-row>
 
+    <!-- 강의 추가 다이얼로그 -->
     <v-dialog v-model="addLectureDialog" max-width="600">
       <v-card>
         <v-card-title>강의 추가</v-card-title>
         <v-card-text>
-          <v-row>
-            <v-col cols="12">
-              <v-text-field v-model="newLecture.name" label="강의명" density="comfortable"></v-text-field>
-              <v-text-field v-model="newLecture.instructor" label="강사명" density="comfortable"></v-text-field>
-              <v-text-field v-model="newLecture.fee" label="수강료" type="number" density="comfortable"></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field v-model="newLecture.startDate" label="시작일" type="date" density="comfortable"></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field v-model="newLecture.endDate" label="종료일" type="date" density="comfortable"></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.MON.checked" label="월요일" value="월요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.MON.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.MON.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.MON.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.MON.checked"></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.TUE.checked" label="화요일" value="화요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.TUE.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.TUE.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.TUE.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.TUE.checked"></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.WED.checked" label="수요일" value="수요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.WED.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.WED.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.WED.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.WED.checked"></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.THU.checked" label="목요일" value="목요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.THU.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.THU.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.THU.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.THU.checked"></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.FRI.checked" label="금요일" value="금요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.FRI.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.FRI.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.FRI.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.FRI.checked"></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.SAT.checked" label="토요일" value="토요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.SAT.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.SAT.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.SAT.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.SAT.checked"></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4">
-                  <v-checkbox v-model="newLecture.schedule.SUN.checked" label="일요일" value="일요일"></v-checkbox>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.SUN.startTime" type="time" label="시작시간"
-                                :disabled="!newLecture.schedule.SUN.checked"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                  <v-text-field v-model="newLecture.schedule.SUN.endTime" type="time" label="종료시간"
-                                :disabled="!newLecture.schedule.SUN.checked"></v-text-field>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
+          <!-- 강의 추가 폼 -->
+          <lecture-form v-model="newLecture" @submit="addLecture"></lecture-form>
         </v-card-text>
         <v-card-actions>
           <v-btn color="primary" @click="addLecture">추가</v-btn>
@@ -156,87 +57,109 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <!-- 강의 수정 다이얼로그 -->
+    <v-dialog v-model="editLectureDialog" max-width="600">
+      <v-card>
+        <v-card-title>강의 수정</v-card-title>
+        <v-card-text>
+          <!-- 강의 수정 폼 -->
+          <lecture-form v-model="editedLecture" @submit="updateLecture"></lecture-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="updateLecture">저장</v-btn>
+          <v-btn color="primary" @click="editLectureDialog = false">취소</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-container>
 </template>
 
 <script setup>
-import {ref, watch} from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import axios from "axios";
+import LectureForm from "@/components/LectureForm.vue";
 
 const addLectureDialog = ref(false)
-const lectures = ref([
-  {
-    id: 1,
-    name: 'Vue.js',
-    instructor: '홍길동',
-    startDate: '2021-09-01',
-    endDate: '2021-09-30',
-    fee: 100000,
-    enrolledStudents: 10,
-  },
-  {
-    id: 2,
-    name: 'React.js',
-    instructor: '김철수',
-    startDate: '2021-10-01',
-    endDate: '2021-10-31',
-    fee: 120000,
-    enrolledStudents: 15,
-  },
-  {
-    id: 3,
-    name: 'Angular.js',
-    instructor: '이영희',
-    startDate: '2021-11-01',
-    endDate: '2021-11-30',
-    fee: 150000,
-    enrolledStudents: 20,
-  },
-])
-
+const editLectureDialog = ref(false)
+const lectures = ref([])
 const newLecture = ref({
   name: '',
+  description: '',
   instructor: '',
   startDate: '',
   endDate: '',
   fee: 0,
   enrolledStudents: 0,
   schedule: {
-    MON: {checked: false, startTime: null, endTime: null},
-    TUE: {checked: false, startTime: null, endTime: null},
-    WED: {checked: false, startTime: null, endTime: null},
-    THU: {checked: false, startTime: null, endTime: null},
-    FRI: {checked: false, startTime: null, endTime: null},
-    SAT: {checked: false, startTime: null, endTime: null},
-    SUN: {checked: false, startTime: null, endTime: null},
+    MON: { checked: false, startTime: null, endTime: null },
+    TUE: { checked: false, startTime: null, endTime: null },
+    WED: { checked: false, startTime: null, endTime: null },
+    THU: { checked: false, startTime: null, endTime: null },
+    FRI: { checked: false, startTime: null, endTime: null },
+    SAT: { checked: false, startTime: null, endTime: null },
+    SUN: { checked: false, startTime: null, endTime: null },
   },
 })
+const editedLecture = ref({ ...newLecture.value })
+
+const fetchLectures = () => {
+  axios.get('https://veritas-s.app/api/lectures').then((response) => {
+    lectures.value = response.data.data
+  })
+}
+
+const openAddLectureDialog = () => {
+  addLectureDialog.value = true
+}
+
+const openEditLectureDialog = (lecture) => {
+  editedLecture.value = { ...lecture }
+  editLectureDialog.value = true
+}
+
+const lectureToDto = (lecture) => {
+  return {
+    name: lecture.name,
+    description: lecture.description,
+    instructor: lecture.instructor,
+    startDate: lecture.startDate,
+    endDate: lecture.endDate,
+    monStartTime: lecture.schedule.MON.checked ? lecture.schedule.MON.startTime : null,
+    monEndTime: lecture.schedule.MON.checked ? lecture.schedule.MON.endTime : null,
+    tueStartTime: lecture.schedule.TUE.checked ? lecture.schedule.TUE.startTime : null,
+    tueEndTime: lecture.schedule.TUE.checked ? lecture.schedule.TUE.endTime : null,
+    wedStartTime: lecture.schedule.WED.checked ? lecture.schedule.WED.startTime : null,
+    wedEndTime: lecture.schedule.WED.checked ? lecture.schedule.WED.endTime : null,
+    thuStartTime: lecture.schedule.THU.checked ? lecture.schedule.THU.startTime : null,
+    thuEndTime: lecture.schedule.THU.checked ? lecture.schedule.THU.endTime : null,
+    friStartTime: lecture.schedule.FRI.checked ? lecture.schedule.FRI.startTime : null,
+    friEndTime: lecture.schedule.FRI.checked ? lecture.schedule.FRI.endTime : null,
+    satStartTime: lecture.schedule.SAT.checked ? lecture.schedule.SAT.startTime : null,
+    satEndTime: lecture.schedule.SAT.checked ? lecture.schedule.SAT.endTime : null,
+    sunStartTime: lecture.schedule.SUN.checked ? lecture.schedule.SUN.startTime : null,
+    sunEndTime: lecture.schedule.SUN.checked ? lecture.schedule.SUN.endTime : null,
+  }
+}
 
 const addLecture = () => {
-  const lectureCreateRequest = {
-    name: newLecture.value.name,
-    description: '', // description 필드가 newLecture에 없으므로 빈 문자열로 설정
-    instructor: newLecture.value.instructor,
-    startDate: newLecture.value.startDate,
-    endDate: newLecture.value.endDate,
-    monStartTime: newLecture.value.schedule.MON.checked ? newLecture.value.schedule.MON.startTime : null,
-    monEndTime: newLecture.value.schedule.MON.checked ? newLecture.value.schedule.MON.endTime : null,
-    tueStartTime: newLecture.value.schedule.TUE.checked ? newLecture.value.schedule.TUE.startTime : null,
-    tueEndTime: newLecture.value.schedule.TUE.checked ? newLecture.value.schedule.TUE.endTime : null,
-    wedStartTime: newLecture.value.schedule.WED.checked ? newLecture.value.schedule.WED.startTime : null,
-    wedEndTime: newLecture.value.schedule.WED.checked ? newLecture.value.schedule.WED.endTime : null,
-    thuStartTime: newLecture.value.schedule.THU.checked ? newLecture.value.schedule.THU.startTime : null,
-    thuEndTime: newLecture.value.schedule.THU.checked ? newLecture.value.schedule.THU.endTime : null,
-    friStartTime: newLecture.value.schedule.FRI.checked ? newLecture.value.schedule.FRI.startTime : null,
-    friEndTime: newLecture.value.schedule.FRI.checked ? newLecture.value.schedule.FRI.endTime : null,
-    satStartTime: newLecture.value.schedule.SAT.checked ? newLecture.value.schedule.SAT.startTime : null,
-    satEndTime: newLecture.value.schedule.SAT.checked ? newLecture.value.schedule.SAT.endTime : null,
-    sunStartTime: newLecture.value.schedule.SUN.checked ? newLecture.value.schedule.SUN.startTime : null,
-    sunEndTime: newLecture.value.schedule.SUN.checked ? newLecture.value.schedule.SUN.endTime : null,
-  };
+  const lectureCreateRequest = lectureToDto(newLecture.value)
 
   axios.post('https://veritas-s.app/api/lectures', lectureCreateRequest).then((response) => {
     console.log(response)
+    addLectureDialog.value = false
+    fetchLectures()
+  })
+}
+
+const updateLecture = () => {
+  const lectureUpdateRequest = lectureToDto(editedLecture.value)
+
+  axios.put(`https://veritas-s.app/api/lectures/${editedLecture.value.id}`, lectureUpdateRequest).then((response) => {
+    console.log(response)
+    editLectureDialog.value = false
+    fetchLectures()
   })
 }
 
@@ -248,6 +171,15 @@ daysOfWeek.forEach(day => {
       newLecture.value.schedule[day].endTime = null
     }
   })
+  watch(() => editedLecture.value.schedule[day].checked, (newVal) => {
+    if (!newVal) {
+      editedLecture.value.schedule[day].startTime = null
+      editedLecture.value.schedule[day].endTime = null
+    }
+  })
 })
 
+onMounted(() => {
+  fetchLectures()
+})
 </script>
